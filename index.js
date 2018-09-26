5e4f2c51762d162009cd0ae835c7f53a47abc9f9
clchenliang <clchenliang@hotmail.com>
2018-09-27 00:52:58 +0800

    feat: 添加 readme

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 #!/usr/bin/env node
-console.log("hello, world23", process.argv[2]);
-// process.on('CL',function(data){
-//     console.log("here get a: ", data);
-// })
-// process.on('END', function(){
-//     process.exit(0);
-// })
 let CONST = {
     "FILETYPE": "UTF-8",
     // 第一个 ### 标题的 # 个数
@@ -17,30 +10,31 @@ let uniqIds = [];
 let indexStr = ["# 目录"];
 let fs = require('fs');
 if(!fs.existsSync(process.argv[2])){
-    console.log("文件不存在")
+    console.log("文件不存在");
+    process.exit(1);
 }
 let data = fs.readFileSync(process.argv[2], CONST.FILETYPE);
 data = data.split("\n");
 console.log(data);
 data.map((line,index)=>{
     if(line.indexOf("#") === 0){
-        console.log(line);
         let newLine = getHn(line);
         data[index] += "<a id=" + newLine.id + "></a>"
         indexStr.push(genContent(newLine));
         // console.log(genContent(getHn(line)));
-        console.log(indexStr)
-        console.log(line, data[index])
     }
 });
 let newData = indexStr.concat(data).join("\n");
 fs.writeFileSync(process.argv[2], newData);
-console.log("添加成功")
+console.log("添加成功，共添加 ",indexStr.length - 1 ," 个目录" )
+
+/**
+ * util 类型函数
+ */
 // 截取字段
 function getHn(line){
     let id = genId();
     let result = {h: line.indexOf(" ") - line.indexOf("#"), t:line.substr(line.indexOf(" ")+1), id:id};
-    line += "<a id=" + id + "></a>"
     return result;
 }
 // 生成 id
---
+++ readme.md
@@ -0,0 +1,11 @@
+## 使用方法
+### 安装
+1. 下载到本地
+2. 进入 `index` 目录
+3. 运行 `npm link`
+4. 即全局安装好了 `addIndex`
+
+### 使用
+```shell
+addIndex fileName
+```
